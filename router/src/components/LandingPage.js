cf0a65b971631b5786feb96d0570a17ec27d85b8
vpyasi11 <you@example.comvpyasi11@gmail.com>
2023-06-03 18:29:27 +0530

    useffect dependency array

--- router/src/components/LandingPage.js
+++ router/src/components/LandingPage.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // use effect
 // hook responsible for side effects (loading after all rendering) like api calling, some kind of dom manipulation
 export default function LandingPage() {
 
+    const [counter, setCounter] = useState(0)
+
     console.log("loads first");
+    // console.log(document.querySelector(".header")) --> wont work as header not yet loaded
 
     // has a callback function and dependency array
     useEffect(()=> {
 
-        console.log("I am useEffect : loads in last after rendering of all other components");
+        console.log("I am useEffect : loads in last after rendering all the content of this component");
+        console.log(document.querySelector(".header"))
 
-    },[])
+    },[counter])
     // [] :dependency array
+        // case 1: dependency array is empty --> useffect runs only singlr time when the page renders
+        // case 2: dependency array has some state or props --> whenever the state or props change, useffect will be trigerred that many times 
 
     console.log("loads second");
 
     return (
-        <div className='header'>LandingPage</div>
+        <div className='header'>
+            <button onClick={()=>{setCounter(counter+1)}}>+</button>
+            {counter}
+        </div>
     )
 }
