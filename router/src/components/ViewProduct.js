62cfbd1917242c058c8d44f09a6ce88be269deae
vpyasi11 <you@example.comvpyasi11@gmail.com>
2023-06-17 19:10:12 +0530

    View product details using params

--- router/src/components/ViewProduct.js
+++ router/src/components/ViewProduct.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { useState } from 'react'
+import '../css/ViewProduct.css'
 
 export default function ViewProduct() {
     const {id} = useParams()
+    const [product, setProduct] = useState([]) 
 
     useEffect(()=>{
         const fetchData = async()=>{
             try{
                 let response=await axios.get(`https://dummyjson.com/products/${id}`)
                 console.log(response.data)
+                setProduct(response.data)
             }
             catch(err){
                 console.log(err);
@@ -20,6 +24,15 @@ export default function ViewProduct() {
     })
 
   return (
-    <div>Product ID : {id}</div>
+    <div class = 'details'>
+        <img src={product.thumbnail}/>
+        <div class='info'>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <h3>${product.price}</h3>
+            <button>BUY NOW</button>
+        </div>
+
+    </div>
   )
 }
